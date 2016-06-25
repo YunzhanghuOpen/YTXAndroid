e11e77e0387d5330ad66418e0ed0af1a489cd214
huangfangyi <84543217@qq.com>
2016-06-25 10:36:05 +0800

    在Text消息类型下分出四类红包相关消息参数

--- app/src/main/java/com/yuntongxun/ecdemo/ui/chatting/ChattingsRowUtils.java
+++ app/src/main/java/com/yuntongxun/ecdemo/ui/chatting/ChattingsRowUtils.java
@@ -13,78 +13,114 @@
 package com.yuntongxun.ecdemo.ui.chatting;
 
 
+import com.yuntongxun.ecdemo.ui.chatting.RedPackUtils.CheckRedPacketMessageUtil;
 import com.yuntongxun.ecdemo.ui.chatting.model.ChattingRowType;
 import com.yuntongxun.ecsdk.ECMessage;
 import com.yuntongxun.ecsdk.ECMeetingManager.ECMeetingType;
 
 /**
  * @author Jorstin Chan@容联•云通讯
- * @date 2014-12-11
  * @version 4.0
+ * @date 2014-12-11
  */
 public class ChattingsRowUtils {
 
     /**
-     *
-     * @param type
+     * @param ecMessage
      * @return
      */
-	public static int getChattingMessageType(ECMessage.Type type) {
-		if(type == ECMessage.Type.TXT) {
-			return 2000;
-		} else if (type == ECMessage.Type.VOICE) {
-			return 60;
-		} else if (type == ECMessage.Type.FILE) {
-			return 1024;
-		} else if (type == ECMessage.Type.IMAGE) {
-			return 200;
-		}else if(type==ECMessage.Type.VIDEO){
-			return 1024;
-		}else if(type==ECMessage.Type.LOCATION){
-			return 2200;
-		}else if(type== ECMessage.Type.CALL){
-			return  2400;
-		}else if(type==ECMessage.Type.RICH_TEXT){
-			return 2600;
-		}
-		return 2000;
-	}
+    public static int getChattingMessageType(ECMessage ecMessage) {
+        ECMessage.Type type = ecMessage.getType();
+        if (type == ECMessage.Type.TXT) {
+            if (CheckRedPacketMessageUtil.isRedPacketMessage(ecMessage) != null) {
 
-	/**
-	 * 
-	 * @param iMessage
-	 * @return
-	 */
-	public static Integer getMessageRowType(ECMessage iMessage) {
-		ECMessage.Type type = iMessage.getType();
-		ECMessage.Direction direction = iMessage.getDirection();
-		if(type == ECMessage.Type.TXT) {
-			if(direction == ECMessage.Direction.RECEIVE) {
-				return ChattingRowType.DESCRIPTION_ROW_RECEIVED.getId();
-			}
-			return ChattingRowType.DESCRIPTION_ROW_TRANSMIT.getId();
-		} else if (type == ECMessage.Type.VOICE) {
-			if(direction == ECMessage.Direction.RECEIVE) {
-				return ChattingRowType.VOICE_ROW_RECEIVED.getId();
-			}
-			return ChattingRowType.VOICE_ROW_RECEIVED.getId();
-		} else if (type == ECMessage.Type.FILE||type==ECMessage.Type.VIDEO) {
-			if(direction == ECMessage.Direction.RECEIVE) {
-				return ChattingRowType.FILE_ROW_RECEIVED.getId();
-			}
-			return ChattingRowType.FILE_ROW_RECEIVED.getId();
-		} else if (type == ECMessage.Type.IMAGE) {
-			if(direction == ECMessage.Direction.RECEIVE) {
-				return ChattingRowType.IMAGE_ROW_RECEIVED.getId();
-			}
-			return ChattingRowType.IMAGE_ROW_RECEIVED.getId();
-		}else if(type==ECMessage.Type.LOCATION){
-			if(direction == ECMessage.Direction.RECEIVE) {
-				return ChattingRowType.LOCATION_ROW_RECEIVED.getId();
-			}
-			return ChattingRowType.LOCATION_ROW_TRANSMIT.getId();
-			
-		}
-		return -1;
-	}
+                return 7000;
+            } else if (CheckRedPacketMessageUtil.isRedPacketAckedMessage(ecMessage) != null) {
+
+                return 8000;
+            }
+
+            return 2000;
+
+
+        } else if (type == ECMessage.Type.VOICE) {
+            return 60;
+        } else if (type == ECMessage.Type.FILE) {
+            return 1024;
+        } else if (type == ECMessage.Type.IMAGE) {
+            return 200;
+        } else if (type == ECMessage.Type.VIDEO) {
+            return 1024;
+        } else if (type == ECMessage.Type.LOCATION) {
+            return 2200;
+        } else if (type == ECMessage.Type.CALL) {
+            return 2400;
+        } else if (type == ECMessage.Type.RICH_TEXT) {
+            return 2600;
+        }
+        return 2000;
+    }
+
+    /**
+     * @param iMessage
+     * @return
+     */
+    public static Integer getMessageRowType(ECMessage iMessage) {
+        ECMessage.Type type = iMessage.getType();
+        ECMessage.Direction direction = iMessage.getDirection();
+        if (type == ECMessage.Type.TXT) {
+
+            if (direction == ECMessage.Direction.RECEIVE) {
+
+
+                if (CheckRedPacketMessageUtil.isRedPacketMessage(iMessage) != null) {
+
+                    return ChattingRowType.REDPACKET_ROW_RECEIVED.getId();
+                } else if (CheckRedPacketMessageUtil.isRedPacketAckedMessage(iMessage) != null) {
+
+                    return ChattingRowType.REDPACKE_ROW_ACK_RECEIVED.getId();
+                }
+
+
+                return ChattingRowType.DESCRIPTION_ROW_RECEIVED.getId();
+            } else {
+
+                if (CheckRedPacketMessageUtil.isRedPacketMessage(iMessage) != null) {
+
+                    return ChattingRowType.REDPACKE_ROW_TO.getId();
+                } else if (CheckRedPacketMessageUtil.isRedPacketAckedMessage(iMessage) != null) {
+
+                    return ChattingRowType.REDPACKE_ROW_ACK_TO.getId();
+                }
+
+
+                return ChattingRowType.DESCRIPTION_ROW_TRANSMIT.getId();
+
+            }
+
+
+        } else if (type == ECMessage.Type.VOICE) {
+            if (direction == ECMessage.Direction.RECEIVE) {
+                return ChattingRowType.VOICE_ROW_RECEIVED.getId();
+            }
+            return ChattingRowType.VOICE_ROW_RECEIVED.getId();
+        } else if (type == ECMessage.Type.FILE || type == ECMessage.Type.VIDEO) {
+            if (direction == ECMessage.Direction.RECEIVE) {
+                return ChattingRowType.FILE_ROW_RECEIVED.getId();
+            }
+            return ChattingRowType.FILE_ROW_RECEIVED.getId();
+        } else if (type == ECMessage.Type.IMAGE) {
+            if (direction == ECMessage.Direction.RECEIVE) {
+                return ChattingRowType.IMAGE_ROW_RECEIVED.getId();
+            }
+            return ChattingRowType.IMAGE_ROW_RECEIVED.getId();
+        } else if (type == ECMessage.Type.LOCATION) {
+            if (direction == ECMessage.Direction.RECEIVE) {
+                return ChattingRowType.LOCATION_ROW_RECEIVED.getId();
+            }
+            return ChattingRowType.LOCATION_ROW_TRANSMIT.getId();
+
+        }
+        return -1;
+    }
 }
