ca9cdc4ebf8c08b7def82b5b4199afde4f530d0f
huangfangyi <84543217@qq.com>
2016-06-30 04:51:14 +0800

    1.进入发红包时增加传入当前用户id 2.群聊红包时将群成员加入红包SDK;

--- app/src/main/java/com/yuntongxun/ecdemo/ui/chatting/ChattingFragment.java
+++ app/src/main/java/com/yuntongxun/ecdemo/ui/chatting/ChattingFragment.java
@@ -745,6 +745,57 @@ public class ChattingFragment extends CCPFragment implements
                     getTopBarView().setTitle(charSequence);
                 }
             }
+            ECGroupManager groupManager = ECDevice.getECGroupManager();
+            // 调用获取群组成员接口，设置结果回调
+            groupManager.queryGroupMembers(ecGroup.getGroupId(),
+                    new ECGroupManager.OnQueryGroupMembersListener() {
+                        @Override
+                        public void onQueryGroupMembersComplete(ECError error
+                                , final List members) {
+                            if (error.errorCode == SdkErrorCode.REQUEST_SUCCESS
+                                    && members != null) {
+                                // 获取群组成员成功
+                                // 将群组成员信息更新到本地缓存中（sqlite） 通知UI更新
+
+                                RPGroupMemberUtil.getInstance().setGroupMemberListener(new NotifyGroupMemberCallback() {
+                                    @Override
+                                    public void getGroupMember(final String groupID, final GroupMemberCallback mCallBack) {
+
+                                        List<RPUserBean> userBeanList = new ArrayList<RPUserBean>();
+
+                                        for (int i = 0; i < members.size(); i++) {
+                                            RPUserBean userBean = new RPUserBean();
+                                            ECGroupMember member=(ECGroupMember) members.get(i);
+                                            userBean.userId =member.getVoipAccount();
+                                            if (userBean.userId.equals(CCPAppManager.getUserId())) {
+                                                continue;
+                                            }
+
+                                            if (member != null) {
+                                                userBean.userAvatar =  "none"  ;
+                                                userBean.userNickname = TextUtils.isEmpty(member.getDisplayName()) ? member.getVoipAccount() :member.getDisplayName();
+                                            } else {
+                                                userBean.userNickname = userBean.userId;
+                                                userBean.userAvatar = "none";
+                                            }
+                                            userBeanList.add(userBean);
+                                        }
+                                        mCallBack.setGroupMember(userBeanList);
+                                    }
+                                });
+
+
+
+                                return;
+                            }
+                            // 群组成员获取失败
+                            Log.e("ECSDK_Demo", "sync group detail fail " +
+                                    ", errorCode=" + error.errorCode);
+
+                        }
+
+                    }
+            );
         }
     }
 
@@ -1742,58 +1793,6 @@ public class ChattingFragment extends CCPFragment implements
             } else {
                 //如果是群聊传递群id和群人数
                 ECGroup ecGroup = GroupSqlManager.getECGroup(mRecipients);
-                ECGroupManager groupManager = ECDevice.getECGroupManager();
-               // 调用获取群组成员接口，设置结果回调
-                groupManager.queryGroupMembers(ecGroup.getGroupId(),
-                        new ECGroupManager.OnQueryGroupMembersListener() {
-                            @Override
-                            public void onQueryGroupMembersComplete(ECError error
-                                    , final List members) {
-                                if (error.errorCode == SdkErrorCode.REQUEST_SUCCESS
-                                        && members != null) {
-                                    // 获取群组成员成功
-                                    // 将群组成员信息更新到本地缓存中（sqlite） 通知UI更新
-
-                                    RPGroupMemberUtil.getInstance().setGroupMemberListener(new NotifyGroupMemberCallback() {
-                                        @Override
-                                        public void getGroupMember(final String groupID, final GroupMemberCallback mCallBack) {
-
-                                            List<RPUserBean> userBeanList = new ArrayList<RPUserBean>();
-
-                                            for (int i = 0; i < members.size(); i++) {
-                                                RPUserBean userBean = new RPUserBean();
-                                                ECGroupMember member=(ECGroupMember) members.get(i);
-                                                userBean.userId =member.getVoipAccount();
-                                                if (userBean.userId.equals(CCPAppManager.getUserId())) {
-                                                    continue;
-                                                }
-
-                                                if (member != null) {
-                                                    userBean.userAvatar =  "none"  ;
-                                                    userBean.userNickname = TextUtils.isEmpty(member.getDisplayName()) ? member.getVoipAccount() :member.getDisplayName();
-                                                } else {
-                                                    userBean.userNickname = userBean.userId;
-                                                    userBean.userAvatar = "none";
-                                                }
-                                                userBeanList.add(userBean);
-                                            }
-                                            mCallBack.setGroupMember(userBeanList);
-                                        }
-                                    });
-
-
-
-                                    return;
-                                }
-                                // 群组成员获取失败
-                                Log.e("ECSDK_Demo", "sync group detail fail " +
-                                        ", errorCode=" + error.errorCode);
-
-                            }
-
-                        }
-                );
-
                 jsonObject.put(RedPacketConstant.KEY_GROUP_ID, ecGroup.getGroupId());
                 jsonObject.put(RedPacketConstant.KEY_GROUO_MEMBERS_COUNT, ecGroup.getCount());
                 jsonObject.put(RedPacketConstant.KEY_CHAT_TYPE, 2);
